08d87561400e3d55e5253de58cfd1351c8996210
Duniealie <162659266+Duniealie@users.noreply.github.com>
2024-09-15 15:13:20 +0700

    sua bai

--- W01Ex12/src/Main.java
+++ W01Ex12/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 5;
+        int b = 5;
+        System.out.println("a cong b = " + a + " " + b);
     }
-} 
+}
